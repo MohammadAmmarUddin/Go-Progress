8aa7192b68bbed5c5cbdffd9badb58ba90d67ab7
Mohammad Ammar <ammaraslam7164@gmail.com>
2025-10-11 19:55:59 +0600

    first up var declare init func

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/*
+tuto-1
+*/
+// func main(){
+
+// 	fmt.Println("hellog")
+
+// }
+
+/* tuto-2  */
+// func add(num1, num2 int){
+
+// 	sum := num1+num2
+
+// 	fmt.Println(sum)
+// }
+
+// func main(){
+
+//     a:=1
+// 	b:=3
+// 	  add(a,b)
+// }
