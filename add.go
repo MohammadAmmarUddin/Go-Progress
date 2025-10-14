86be7366d0b4d24c289c30aa75d4066e96460a7c
Mohammad Ammar <ammaraslam7164@gmail.com>
2025-10-14 13:12:58 +0600

    2 page func

---
+++ add.go
@@ -0,0 +1,6 @@
+package main
+func Add(n1 int, n2 int) int {
+
+	sum := n1 + n2
+	return sum
+}
--- main.go
+++ main.go
@@ -2,41 +2,11 @@ package main
 
 import "fmt"
 
-/*
-tuto-1
-*/
-// func main(){
-
-// 	fmt.Println("hellog")
-
-// }
-
-/* tuto-2  */
-func add(num1, num2 int){
-
-	sum := num1+num2
-
-	fmt.Println(sum)
-}
-
-/*
- func return value
-*/
-
-
-func getNumber(num1 int, num2 int) (int,int) {
-
- sum := num1 + num2 
- mul:= num1 * num2
-
- return sum,mul
-}
-
 func main(){
+  
 
-    a:=1
-	b:=3
-	  add(a,b)
-	  fmt.Println(getNumber(2,3))
+
+
+ fmt.Println(Add(3,4))
 }
 
